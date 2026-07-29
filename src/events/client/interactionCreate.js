cdd00fbe3ecda750db0d81ce306fafecbf116c5c
Jumicode <jromeroacive@gmail.com>
2026-07-29 11:44:19 -0400

    feat: manejador de interacciones implementado

---
+++ src/events/client/interactionCreate.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction, client) {
+        if (!interaction.isChatInputCommand()) return;
+
+        const command = client.commands.get(interaction.commandName);
+
+        if (!command) return;
+
+        try {
+            await command.execute(interaction, client);
+        } catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({ content: 'Hubo un error ejecutando el comando.', ephemeral: true });
+            } else {
+                await interaction.reply({ content: 'Hubo un error ejecutando el comando.', ephemeral: true });
+            }
+        }
+    },
+};
